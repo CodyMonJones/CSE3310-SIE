a4d0f48ff246f42648fd851054875678a03bdf68
Gustavo <chavezgustavo813@gmail.com>
2022-10-26 20:27:02 -0500

    removed comments from login and added button functionality

---
+++ CSE3310Project/app/src/main/java/com/example/cse3310project/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.cse3310project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText email;
+    EditText password;
+    MaterialButton loginButton;
+    MaterialButton registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.Email);
+        password = (EditText)  findViewById(R.id.Password);
+        loginButton = (MaterialButton) findViewById(R.id.LoginButton);
+        registerButton = (MaterialButton) findViewById(R.id.RegisterButton);
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.RegisterButton:
+                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+                break;
+        }
+    }
+}
