34e1b56cff43b648c3dfe9fb5a5b4e4d7d525e92
Gustavo <chavezgustavo814@gmail.com>
2022-10-27 02:28:35 -0500

    finished up linking of login and registration page

--- CSE3310Project/app/src/main/java/com/example/cse3310project/LoginActivity.java
+++ CSE3310Project/app/src/main/java/com/example/cse3310project/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -26,8 +28,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         email = (EditText) findViewById(R.id.Email);
         password = (EditText)  findViewById(R.id.Password);
         loginButton = (MaterialButton) findViewById(R.id.LoginButton);
+
+        email.addTextChangedListener(new emailWatcher());
+        password.addTextChangedListener(new emailWatcher());
+
         registerButton = (MaterialButton) findViewById(R.id.RegisterButton);
         registerButton.setOnClickListener(this);
+    }
+
+    private class emailWatcher implements TextWatcher
+    {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+        {
+            String userEmail = email.getText().toString();
+            String userPassword = password.getText().toString();
+
+            loginButton.setEnabled(!userEmail.isEmpty() && !userPassword.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) 
+        {
+
+        }
     }
 
     @Override
--- CSE3310Project/app/src/main/java/com/example/cse3310project/RegistrationActivity.java
+++ CSE3310Project/app/src/main/java/com/example/cse3310project/RegistrationActivity.java
@@ -2,13 +2,41 @@ package com.example.cse3310project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class RegistrationActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
+
+    MaterialButton cancelButton;
+    MaterialButton submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        cancelButton = (MaterialButton) findViewById(R.id.CancelButton);
+        submitButton = (MaterialButton) findViewById(R.id.SubmitButton);
+
+        submitButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+
+        submitButton.setEnabled(false);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.CancelButton:
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+                break;
+        }
     }
 }
