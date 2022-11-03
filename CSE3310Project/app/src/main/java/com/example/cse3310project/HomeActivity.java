208d5e76aeb10b816336c25e6654962c342cbf51
Cody Jones <Cody.Monroe.Jones@gmail.com>
2022-11-03 14:50:26 -0500

    adding click functionality to NavMenu

--- CSE3310Project/app/src/main/java/com/example/cse3310project/HomeActivity.java
+++ CSE3310Project/app/src/main/java/com/example/cse3310project/HomeActivity.java
@@ -1,15 +1,29 @@
 package com.example.cse3310project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,)
     }
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 }
