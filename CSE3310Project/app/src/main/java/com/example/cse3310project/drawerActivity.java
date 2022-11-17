801b3eeb377ed4ad1318c9cc46eef0834d8e9393
Cody Jones <Cody.Monroe.Jones@gmail.com>
2022-11-17 12:09:25 -0600

    fixed nav and activities views

---
+++ CSE3310Project/app/src/main/java/com/example/cse3310project/drawerActivity.java
@@ -0,0 +1,64 @@
+package com.example.cse3310project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class drawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    static DrawerLayout drawerLayout;
+
+    @Override
+    public void setContentView(View view){
+        drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_drawer, null);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        container.addView(view);
+        super.setContentView(drawerLayout);
+
+        Toolbar toolbar = drawerLayout.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = drawerLayout.findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.Home:
+                startActivity(new Intent(this, HomeActivity.class));
+                overridePendingTransition(0, 0);
+                break;
+
+            case R.id.Clubs:
+                startActivity(new Intent(this, FormClubActivity.class));
+                overridePendingTransition(0,0);
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    protected void allocateActivityTitle(String titleString){
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle(titleString);
+        }
+    }
+}
